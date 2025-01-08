78574d3978a441f042084945f7888a10e3404650
Arman Minasyan <juzojuzo23@gmail.com>
2025-01-08 23:53:18 +0400

    first comment

--- script/index.js
+++ script/index.js
@@ -1,4 +1,60 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let currentLanguage = "en";
+  const switchButton = document.getElementById("switchLng");
+  const usaFlag = document.getElementById("usaLng");
+  const georgiaFlag = document.getElementById("geLng");
+
+  switchButton.addEventListener("click", () => {
+    if (currentLanguage === "en") {
+      currentLanguage = "ge";
+      usaFlag.style.display = "none";
+      georgiaFlag.style.display = "block";
+    } else {
+      currentLanguage = "en";
+      usaFlag.style.display = "block";
+      georgiaFlag.style.display = "none";
+    }
+
+    console.log(`Language switched to: ${currentLanguage}`);
+    // Добавьте сюда ваш код для смены языка, например:
+    // changeLanguage(currentLanguage);
+  });
+
+  const burger = document.querySelector(".burger");
+  const navPanel = document.querySelector(".nav-panel");
+  const container = document.querySelector(".container");
+  const navLinks = document.querySelectorAll(".nav a");
+  const header = document.querySelector(".header");
+
+  // Открытие/закрытие меню при клике на бургер
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active"); // Переключаем класс для бургер-меню (полоски на крестик)
+    navPanel.classList.toggle("open"); // Открываем или закрываем навигационную панель
+    container.classList.toggle("locked"); // Блокируем или разблокируем скроллинг
+  });
+
+  // Закрытие меню при клике на ссылку
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      burger.classList.remove("active");
+      navPanel.classList.remove("open");
+      container.classList.remove("locked"); // Снимаем блокировку скроллинга
+    });
+  });
+
+  // Закрытие меню при клике вне бургер-меню и панели навигации
+  document.addEventListener("click", (e) => {
+    if (
+      !header.contains(e.target) &&
+      !navPanel.contains(e.target) &&
+      !burger.contains(e.target)
+    ) {
+      burger.classList.remove("active");
+      navPanel.classList.remove("open");
+      container.classList.remove("locked"); // Снимаем блокировку скроллинга
+    }
+  });
+
   const buttons = document.querySelectorAll(".item button");
 
   buttons.forEach((button) => {
